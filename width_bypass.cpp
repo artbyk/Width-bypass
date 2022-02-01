93138842422e6fc7cd75b849fbf5b61c8d49e386
artbyk <71276254+artbyk@users.noreply.github.com>
2022-02-01 11:57:58 +0200

    Update width_bypass.cpp

--- width_bypass.cpp
+++ width_bypass.cpp
@@ -1,56 +1,70 @@
 #include <iostream>
-using namespace std;
 #include <vector>
-#include <algorithm>    
-#include <math.h>
+#include <queue>
+#include <algorithm>
+using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    int n,m,s;
-    cin>>n>>m>>s;
-    if(m == s){
-        cout<<0;
-        return 0;
-    }
-    int **arr = new int*[n];
-    for(int i =0;i<n;i++){
-        arr[i] = new int[n];
-        for(int q = 0; q< n;q++){
-            cin>>arr[i][q];
+    int n; // Число вершин
+    int s; // Стартовая вершина (вершины везде нумеруются с нуля)
+    int to; // Кратчайший путь до какой-то вершины to
+    
+    cin>> n >> s >> to;
+    
+    vector < vector <int> > g(n, vector <int> (n) );
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            cin >> g[i][j]; 
         }
     }
-    vector<int> vec;
-    vector<int> vec_buf;
-    vec_buf.push_back(0);
-    vec.push_back(m-1);
-    int buf, flag = 0;
-    std::vector<int>::iterator it;
-    while(true){
-        buf = vec[flag];
-        for(int i = 0; i< n;i++){
-            if(arr[buf][i] == arr[i][buf] && arr[buf][i] == 1){
-                if(i == (s-1)){
-                    cout<<1+vec_buf[flag];
-                    return 0;
-                }
-                it = find(vec.begin(), vec.end(), i);
-                if(it != vec.end()){
-                    continue;
-                }
-                else{
-                    vec.push_back(i);
-                    vec_buf.push_back(vec_buf[flag] + 1);
-                }
+    
+    
+    // Сам обход
+    queue<int> q;
+    q.push (s);
+    vector<bool> used (n);
+    vector<int> d (n), p (n);
+    used[s] = true;
+    p[s] = -1;
+    while (!q.empty()) 
+    {
+        int v = q.front();
+        q.pop();
+        for (size_t i=0; i<g[v].size(); ++i) 
+        {
+            int to = g[v][i];
+            if (!used[to]) 
+            {
+                used[to] = true;
+                q.push (to);
+                d[to] = d[v] + 1;
+                p[to] = v;
             }
         }
-        if(flag >= vec.size()){
-            cout<<0;
-            return 0;
+    }
+    
+    // вывести кратчайший путь до какой-то вершины to
+    if (!used[to])
+    {
+        cout << "No path!";
+    }
+    else 
+    {
+        vector<int> path;
+        for (int v=to; v!=-1; v=p[v])
+        {
+            path.push_back (v);
         }
-        flag++;
+        reverse (path.begin(), path.end());
+        cout << "Path: ";
+        for (size_t i=0; i<path.size(); ++i)
+        {
+            cout << path[i] + 1 << " ";
+        }
     }
-    for(int i =0;i<n;i++){
-        delete[] arr[i];
-    }
+    
+    return 0;
 }
